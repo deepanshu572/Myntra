c9faecb955f1e2e8e6fa6ed11029359397d67286
deepanshu572 <142653242+deepanshu572@users.noreply.github.com>
2025-06-13 12:12:14 +0530

    minor2 change

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import HeroSec from "../../Components/heroSec/HeroSec";
+import HeroSec from "../../Components/HeroSec/HeroSec";
 import HomeCategorySec from "../../Components/HomeCategorySec/HomeCategorySec";
 import HomeProductSec from "../../Components/HomeProductSec/HomeProductSec";
 
